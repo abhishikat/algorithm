e5b616e92a2522e4beaf9a94cfdf9e71d1f95a8d
abhishikat <53806537+abhishikat@users.noreply.github.com>
2020-03-15 10:43:37 +0530

    Update LargestElementInArray.c
    klargest element in an array

--- LargestElementInArray.c
+++ LargestElementInArray.c
@@ -1,50 +1,44 @@
-// print k largest element in an array;
 
-
+/*Write an efficient program for printing k largest elements in an array. Elements in the array can be in any order. It is given that all array elements are distinct.*/
 #include<stdio.h>
-//kth largest element in array
+#include<time.h>
+void bubblesort(int arr[],int n)
+{	int temp, k;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(arr[j]<arr[j+1])
+			{
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	printf("Enter how many largest elements you want to print:");
+	scanf("%d",&k);
 
-int main(void)
-{
-    int n,a,t,arr[100];
-    scanf("%d",&t);
-    while(t--)
-    {
-        scanf("%d",&n);
-        for(int i=0;i<n;i++)
-        {
-            scanf("%d",&arr[i]);
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]<arr[i+1])
-            {
-                a=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=a;
-            }
-        }
-for(int i=0;i<n;i++)
-{
-printf("%d",arr[i]);
+	for(int i=0;i<k;i++)
+	{
+		printf("%d",arr[i]);
+	}
+
 }
 
-	int k;
-scanf("%d",&k);
-for(int i=0;i<k;i++)
-{
-printf("%d\n",arr[i]);
-}
-
-
-
-
-	 for(int i=0;i<n;i++)
-        {
-        printf("LARGEST ELEMENT IN ARRAY IS %d\n", arr[n-1]);
+int main()
+{	int n,ch;
+	clock_t start, end; ;
+	printf("Enter number of elements:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter array elements\n ");
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	bubblesort(arr,n);
 	
-        }
-    }
-return 0;
+  
+	return 0;
 }
-
